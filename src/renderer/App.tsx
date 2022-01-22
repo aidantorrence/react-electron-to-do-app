6a5e207adcf79e4785c485e131a14dc2954a54bd
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-21 22:49:26 -0500

    add reader styling

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 
 export default function App() {
-  const [inputValue, setInputValue] = useState(`set up electron app <=> set up nextJS portfolio <=> reddit board that shows top to-dos linked to their site of choice (rewards good to do ers with a link to their website of choice)`)
-  return <div className="main">
-    <textarea className="text-area" 
-      onChange={e => setInputValue(e.target.value)}
-      value={inputValue}
-      spellcheck="false"
-    />
-
+  const [inputValue, setInputValue] = useState(
+    `set up electron app <=> set up nextJS portfolio <=> reddit board that shows top to-dos linked to their site of choice (rewards good to do ers with a link to their website of choice)`
+  );
+  return (
+    <div className="main">
+      <textarea
+        className="text-area"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        spellCheck="false"
+      />
     </div>
+  );
 }
